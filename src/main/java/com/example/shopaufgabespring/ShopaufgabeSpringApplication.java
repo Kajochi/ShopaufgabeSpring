a74e4691540478049ab18f5d91b8fd1af17a914f
christophgroneberg <christophgroneberg1@gmx.de>
2023-08-23 12:29:28 +0200

    Initial commit

---
+++ src/main/java/com/example/shopaufgabespring/ShopaufgabeSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.shopaufgabespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopaufgabeSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopaufgabeSpringApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/shopaufgabespring/model/ShopController.java
@@ -0,0 +1,2 @@
+package com.example.shopaufgabespring.model;public class ShopController {
+}
